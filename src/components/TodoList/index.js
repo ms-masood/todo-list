98bd2f2884ac02858da8697a81a8731142e91b95
Shoaib Masood <ms.masood037@gmail.com>
2021-04-15 03:01:57 +0500

    Added basic structure of application components

---
+++ src/components/TodoList/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {}
+	}
+
+	render() {
+		return (
+			<h1>List</h1>
+		)
+	}
+}
+
+export default TodoList;
